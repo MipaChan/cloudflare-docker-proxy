9b0363db747a32aed6da755ada26ceae83370d1f
MipaChan <98219327+MipaChan@users.noreply.github.com>
2024-01-13 22:11:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function routeByHosts(host) {
 
 async function handleRequest(request) {
   const url = new URL(request.url);
-  console.log(request);
+  console.log(JSON.stringify(request));
   const upstream = routeByHosts(url.hostname);
   if (upstream === "") {
     return new Response(
